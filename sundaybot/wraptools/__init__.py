b022b80b6a0c051506f02b252d9ed19d52c0a01f
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-03-19 10:20:04 +0530

    Update __init__.py

--- sundaybot/wraptools/__init__.py
+++ sundaybot/wraptools/__init__.py
@@ -1,15 +1,4 @@
-#    Copyright (C) Midhun KM 2020-2021
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 
 import functools
@@ -17,9 +6,9 @@ import re
 
 from telethon import events
 
-from fridaybot import bot
-from fridaybot.Configs import Config
-from fridaybot.Configs import Config
+from sundaybot import bot
+from sundaybot.Configs import Config
+from sundaybot.Configs import Config
 
 
 def ignore_fwd():
