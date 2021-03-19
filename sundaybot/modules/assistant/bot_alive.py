660ca8ca098149fbf11b8558da02cfb1071ebe3f
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-03-19 09:56:11 +0530

    Update bot_alive.py

--- sundaybot/modules/assistant/bot_alive.py
+++ sundaybot/modules/assistant/bot_alive.py
@@ -1,25 +1,14 @@
-#    Copyright (C) Midhun KM 2020-2021
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from fridaybot.Configs import Config
+
+from sundaybot.Configs import Config
 import time
 from telethon import __version__ as tv
 import sys
 import platform
 from git import Repo
-from fridaybot import ALIVE_NAME
-from fridaybot import bot
-from fridaybot.modules import currentversion
+from sundaybot import ALIVE_NAME
+from sundaybot import bot
+from sundaybot.modules import currentversion
 
 # only Owner Can Use it
 @assistant_cmd("alive", is_args=False)
