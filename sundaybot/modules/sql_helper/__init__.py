9f2ac38df473ca6c9242ed0c4531f8563e787949
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-03-19 09:34:55 +0530

    Update __init__.py

--- sundaybot/modules/sql_helper/__init__.py
+++ sundaybot/modules/sql_helper/__init__.py
@@ -3,6 +3,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from fridaybot.db_start import BASE, SESSION
-from fridaybot.Configs import Config
+from sundaybot.db_start import BASE, SESSION
+from sundaybot.Configs import Config
 # -------------------------------------- #
