5c8a5220dfeb51867d3661dedb91d310f41af16d
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-03-19 09:49:11 +0530

    Update server_pinger_sql.py

--- sundaybot/modules/sql_helper/server_pinger_sql.py
+++ sundaybot/modules/sql_helper/server_pinger_sql.py
@@ -1,19 +1,8 @@
-#    Copyright (C) Midhun KM 2020-2021
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 from sqlalchemy import Column, UnicodeText
 
-from fridaybot.modules.sql_helper import BASE, SESSION
+from sundaybot.modules.sql_helper import BASE, SESSION
 
 
 class Serverpinger(BASE):
@@ -41,9 +30,9 @@ def rmping(url):
 
 
 def get_all_url():
-    stark = SESSION.query(Serverpinger).all()
+    sunday = SESSION.query(Serverpinger).all()
     SESSION.close()
-    return stark
+    return sunday
 
 
 def is_ping_indb(url):
