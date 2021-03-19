45bd7435080407ee867b4fae52ac1fe5457cd75e
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-03-19 11:43:28 +0530

    Update amazon_stark_tracker.py

--- sundaybot/modules/amazon_stark_tracker.py
+++ sundaybot/modules/amazon_stark_tracker.py
@@ -1,23 +1,12 @@
-#    Copyright (C) Midhun KM 2020-2021
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from fridaybot.modules.sql_helper.amazon_tracker_sql import add_new_tracker, is_tracker_in_db, rm_tracker, get_tracker_info, rm_tracker, get_all_tracker, get_all_urls
+from sundaybot.modules.sql_helper.amazon_tracker_sql import add_new_tracker, is_tracker_in_db, rm_tracker, get_tracker_info, rm_tracker, get_all_tracker, get_all_urls
 import requests
 from bs4 import BeautifulSoup
 
-@friday.on(friday_on_cmd(pattern="amt"))
+@sunday.on(sunday_on_cmd(pattern="amt"))
 async def _(event):
     if event.fwd_from:
         return
@@ -41,7 +30,7 @@ async def _(event):
     add_new_tracker(url, price)
     await event.edit(f"Product Name : {title} \nCurrent Price : {price} \n**Added To TrackerList**")
     
-@friday.on(friday_on_cmd(pattern="rmt"))
+@sunday.on(sunday_on_cmd(pattern="rmt"))
 async def _(event):
     if event.fwd_from:
         return
