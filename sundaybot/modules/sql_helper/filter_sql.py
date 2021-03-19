9b16ed4b4003aad5294a894f02fc1fcdc31267e8
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-03-19 09:42:30 +0530

    Update filter_sql.py

--- sundaybot/modules/sql_helper/filter_sql.py
+++ sundaybot/modules/sql_helper/filter_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, LargeBinary, Numeric, String, UnicodeText
 
-from fridaybot.modules.sql_helper import BASE, SESSION
+from sundaybot.modules.sql_helper import BASE, SESSION
 
 
 class Filters(BASE):
