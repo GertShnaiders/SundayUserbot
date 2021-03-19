70a4fe173476b9a6d02463648aa4e4cfb1284797
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-03-19 09:49:47 +0530

    Update snips_sql.py

--- sundaybot/modules/sql_helper/snips_sql.py
+++ sundaybot/modules/sql_helper/snips_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, LargeBinary, Numeric, UnicodeText
 
-from fridaybot.modules.sql_helper import BASE, SESSION
+from sundaybot.modules.sql_helper import BASE, SESSION
 
 
 class Snips(BASE):
