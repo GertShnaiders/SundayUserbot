2c236bde6a4d2a2e63192d485785acc52d2c719c
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-03-19 09:47:08 +0530

    Update nsfw_watch_sql.py

--- sundaybot/modules/sql_helper/nsfw_watch_sql.py
+++ sundaybot/modules/sql_helper/nsfw_watch_sql.py
@@ -1,18 +1,7 @@
-#    Copyright (C) Midhun KM 2020-2021
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from fridaybot.modules.sql_helper import BASE, SESSION
+from sundaybot.modules.sql_helper import BASE, SESSION
 
 
 class Nsfwatch(BASE):
@@ -40,9 +29,9 @@ def rmnsfwatch(chat_id: str):
 
 
 def get_all_nsfw_enabled_chat():
-    stark = SESSION.query(Nsfwatch).all()
+    sunday = SESSION.query(Nsfwatch).all()
     SESSION.close()
-    return stark
+    return sunday
 
 
 def is_nsfwatch_indb(chat_id: str):
