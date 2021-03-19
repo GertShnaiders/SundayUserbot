7dd7890e26557557ccf46f782d819c767c4d80bb
GertShnaiders <79639379+GertShnaiders@users.noreply.github.com>
2021-03-19 09:43:53 +0530

    Update idadder_sql.py

--- sundaybot/modules/sql_helper/idadder_sql.py
+++ sundaybot/modules/sql_helper/idadder_sql.py
@@ -1,15 +1,4 @@
-#    Copyright (C) Midhun KM 2020-2021
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 
 from sqlalchemy import Column, String
@@ -35,9 +24,9 @@ def add_usersid_in_db(chat_id: int):
 
 
 def get_all_users():
-    stark = SESSION.query(Moidata).all()
+    sunday = SESSION.query(Moidata).all()
     SESSION.close()
-    return stark
+    return sunday
 
 
 def already_added(chat_id):
